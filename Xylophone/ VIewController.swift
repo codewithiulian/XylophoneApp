502d085f45ad253ff6330223bebf3a12a77b8188
Iulian Oana <iulian.andreioana@gmail.com>
2019-07-29 17:35:56 +0100

    Initial app setup

---
+++ Xylophone/ VIewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Iulian Oana on 29/07/2019.
+//  Copyright © 2019 Iulian Oana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController{
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+
+    @IBAction func notePressed(_ sender: UIButton) {
+        
+        
+        
+    }
+    
+  
+
+}
+
